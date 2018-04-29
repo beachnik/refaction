bd37f11a6c2c0584c80fbf18b1ffc01932b4a95e
Nick Beach <beachnik2002@gmail.com>
2018-04-29 17:51:41 +1200

    More unit tests.

--- refactor-unittests/ProductRepositoryTests.cs
+++ refactor-unittests/ProductRepositoryTests.cs
@@ -41,5 +41,71 @@ namespace refactor_unittests
 
             Assert.AreEqual("Newest mobile product from Samsung.", results[0].Description);
         }
+
+        [TestMethod]
+        public void GetByNameFailureTest()
+        {
+            ProductRepository repo = new ProductRepository();
+
+            List<Product> results = repo.GetByName("Banana");
+
+            Assert.IsTrue(results.Count == 0);
+        }
+
+        [TestMethod]
+        public void GetAllTest()
+        {
+            ProductRepository repo = new ProductRepository();
+
+            List<Product> results = repo.GetAll();
+
+            Assert.IsTrue(results.Count == 2);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            ProductRepository repo = new ProductRepository();
+
+            Product test = repo.GetByID(Guid.Parse("8f2e9176-35ee-4f0a-ae55-83023d2db1a3"));
+
+            string description = test.Description;
+
+            test.Description = "This is changed for a unit test";
+
+            repo.Update(test);
+
+            Product postUpdate = repo.GetByID(Guid.Parse("8f2e9176-35ee-4f0a-ae55-83023d2db1a3"));
+
+            Assert.AreEqual(test.Description, postUpdate.Description);
+
+            postUpdate.Description = description;
+
+            repo.Update(postUpdate);
+        }
+
+        [TestMethod]
+        public void AddAndDeleteTest()
+        {
+            ProductRepository repo = new ProductRepository();
+            Guid id = new Guid();
+            Product Test = new Product(id, "TestProduct", "This is a test product for unit testing purposes", 1, 2);
+
+            repo.Insert(Test);
+
+            Product postInsert = repo.GetByID(id);
+
+            Assert.AreEqual(Test.Id, postInsert.Id);
+            Assert.AreEqual(Test.Name, postInsert.Name);
+            Assert.AreEqual(Test.Description, postInsert.Description);
+            Assert.AreEqual(Test.Price, postInsert.Price);
+            Assert.AreEqual(Test.DeliveryPrice, postInsert.DeliveryPrice);
+
+            repo.Delete(postInsert);
+
+            Product postDelete = repo.GetByID(id);
+
+            Assert.IsNull(postDelete);
+        }
     }
 }
