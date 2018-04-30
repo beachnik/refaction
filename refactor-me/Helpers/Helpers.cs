d52da79133392e4e713fdc6a162af478d049cba5
Nick Beach <beachnik2002@gmail.com>
2018-04-30 21:54:01 +1200

    A fix to make sure the unit tests can work on someone machine who's not me.

--- refactor-me/Helpers/Helpers.cs
+++ refactor-me/Helpers/Helpers.cs
@@ -10,13 +10,16 @@ namespace refactor_me.Models
         public static SqlConnection NewConnection()
         {
             string connstr = "";
+            
             //A bit of a dirty hack to let me set up some unit testing
             if (HttpContext.Current != null)
             {
                 connstr = ConnectionString.Replace("{DataDirectory}", HttpContext.Current.Server.MapPath("~/App_Data"));   
             } else
             {
-                connstr = ConnectionString.Replace("{DataDirectory}", @"C:\Users\beach\Source\Repos\refaction\refactor-unittests\App_Data");
+                string unitTestFileLocation = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+                unitTestFileLocation.Substring(0, unitTestFileLocation.IndexOf(@"\bin\"));
+                connstr = ConnectionString.Replace("{DataDirectory}", unitTestFileLocation + @"\App_Data");
             }
             return new SqlConnection(connstr);
         }
